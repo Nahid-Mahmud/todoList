20b9cc15036a1b8f2b6bb892a18d7048717afa0c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-08-15 00:05:31 +0600

    Added contion for disabled button

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
+"use strict ";
+document
+  .getElementById("user-input")
+  .addEventListener("keyup", function (event) {
+    const text = event.target.value;
+    if (text !== "") {
+      document.querySelector("#submit").removeAttribute("disabled");
+    } else {
+      document.querySelector("#submit").setAttribute("disabled", true);
+    }
+  });
+
 document.getElementById("submit").addEventListener("click", function () {
   const inputValue = document.querySelector("#user-input").value;
   // console.log(inputValue);
   const li = document.createElement("li");
   // console.log(li);
+  li.setAttribute("class", "listItem");
+  li.classList.add(
+    "listItem",
+    "w-[max-content]",
+    "text-xl",
+    "px-4",
+    "py-4",
+    "rounded-md",
+    "text-center",
+    "flex",
+    "flex-col",
+    "bg-slate-400"
+  );
+  // console.log(li);
   li.innerText = inputValue;
   // console.log(li)
+
   document.querySelector("#list-container").appendChild(li);
 });
+
+document
+  .querySelector("#list-container")
+  .addEventListener("click", function (event) {
+    // console.log(event.target);
+    event.target.parentNode.removeChild(event.target);
+  });
